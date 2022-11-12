899c2aca814169f3830d76e0c159139bcde9c389
Valentin-Dimitrie Popescu <popescuvalentin16768@gmail.com>
2022-11-13 00:56:55 +0200

    extend functionality

--- components/chart.m
+++ components/chart.m
@@ -1,9 +1,16 @@
-function chart(fig,ActionText,amplitude,isJitter,isGaussian,isEcho,isAwg)
+function chart(fig,microphone,ActionText,amplitude,periods,isJitter,isGaussian,isEcho,isAwg)
 factorAmplificareJitter = 500;
 gaussianSnr=20; 
+
+if(microphone)
 ActionText.String='Inregistreaza';
 originalSignal = audioRecorder();
 editedSignal = originalSignal;
+else
+ActionText.String='Semnal de test';
+originalSignal = sin(0:0.1:6*periods);
+editedSignal = originalSignal;
+end
 
 if(amplitude&&amplitude~=1)
     editedSignal = editedSignal.*amplitude;
@@ -20,6 +27,7 @@ end
 if(isEcho)
     editedSignal=echo_signal(editedSignal,44100,0.3,0.5);
 end
+
 if(isAwg)
     editedSignal=smoothing(editedSignal);
 end
@@ -29,6 +37,11 @@ originalChartUI = uiaxes(fig, 'Position',[650 500 550 400]);
 
 editedChartUI = uiaxes(fig, 'Position',[650 100 550 400]);
  plot(editedChartUI,editedSignal);
-ActionText.String='Reda';
- soundsc(double(editedSignal),44100,16);
+
+
+if(microphone) 
+    ActionText.String='Reda';
+    soundsc(double(editedSignal),44100,16);
+end
+
 end
--- main.m
+++ main.m
@@ -1,13 +1,15 @@
 function main()
 clear all;
 close;
+microphone=0;
 isPause=false;
 amplitude=1;
+periods=10;
 isJitter=0;
 isGaussian=0;
 isEcho=0;
 isAwg=0;
 
 addpath("components\");
-stylesheet(isPause,amplitude,isJitter,isGaussian,isEcho,isAwg);
+stylesheet(microphone,isPause,amplitude,periods,isJitter,isGaussian,isEcho,isAwg);
 end
